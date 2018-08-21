d77957e504d30b12207e54a8fbfec30ab193580d
Gidaio <killshade@gmail.com>
2018-08-21 17:41:54 -0600

    start over

--- build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo Linting TypeScript...
-tslint -p .
-echo Compiling JavaScript...
-tsc -p .
-echo Copying HTML...
-mkdir -p build/frontend
-cp -r src/frontend/*.{html,css} build/frontend
